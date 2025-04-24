b6af36cdcf4123621dee9d6a40aaa937171c2309
sofiia koval <kovalsofiia1@gmail.com>
2025-04-25 01:13:38 +0300

    lab2: add filters

--- base/views.py
+++ base/views.py
@@ -26,23 +26,24 @@ def welcome(request):
 
 @login_required
 def task_list(request):
-    search_query = request.GET.get('search', '')
-    selected_col = request.GET.get('collection')
+    query = request.GET.get('query', '')  # Отримуємо пошуковий запит
+    collection_filter = request.GET.get('collection', '')  # Отримуємо параметр фільтрації по колекції (як рядок)
 
-    tasks = Task.objects.filter(owner=request.user)
+    tasks = Task.objects.all()
 
-    if search_query:
-        tasks = tasks.filter(title__icontains=search_query)
-    if selected_col:
-        tasks = tasks.filter(collection__id=selected_col)
+    if query:
+        tasks = tasks.filter(title__icontains=query)  # Пошук по назві задачі
 
+    if collection_filter:
+        tasks = tasks.filter(collection_id=collection_filter)  # Фільтрація по колекції
+
+    collections = Collection.objects.all()  # Отримуємо всі колекції для фільтрації
     return render(request, 'base/task_list.html', {
         'tasks': tasks,
-        'search': search_query,
-        'collections': Collection.objects.filter(owner=request.user),
-        'selected_col': selected_col
+        'collections': collections,
+        'query': query,
+        'collection_filter': collection_filter  # передаємо як рядок
     })
-
 
 @login_required
 def add_task(request):
@@ -143,37 +144,11 @@ def collection_list(request):
     return render(request, 'base/collection_list.html', {'collections': collections})
 
 
-# @login_required
-# def add_collection(request):
-#     if request.method == 'POST':
-#         name = request.POST['name']
-#         Collection.objects.create(name=name, owner=request.user)
-#         return redirect('collection_list')
-#     return render(request, 'base/collection_form.html')
-#
-
 @login_required
 def delete_collection(request, col_id):
     collection = get_object_or_404(Collection, id=col_id, owner=request.user)
     collection.delete()
     return redirect('collection_list')
-
-
-# @login_required
-# def edit_collection(request, col_id):
-#     collection = get_object_or_404(Collection, id=col_id, owner=request.user)
-#
-#     if request.method == 'POST':
-#         collection.name = request.POST['name']
-#         collection.save()
-#         return redirect('collection_list')
-#
-#     return render(request, 'base/collection_form.html', {'collection': collection})
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
-from .models import Collection
-from django.contrib.auth.decorators import login_required
-
 
 @login_required
 def add_or_edit_collection(request, col_id=None):
